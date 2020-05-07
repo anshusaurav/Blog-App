a73ff52c4fa6d87976106ddf3d01ca41c72c0d46
anshusaurav <anshu.saurav@gmail.com>
2020-05-07 23:24:27 +0530

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express-Gen-Blog' });
+  res.render('index', { title: 'Article-Store' });
 });
 
 module.exports = router;
